4f57112c4712b7103805e7202ecddaf52b75455b
Laurent <lwieser@capcod.eu>
2021-12-07 10:33:46 +0100

    refactroing

---
+++ Tacos/MenuItemFactory.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Tacos
+{
+    public class MenuItemFactory
+    {
+        public static List<MenuItem> GetItems()
+        {
+            var ts = new TacosService();
+            return new List<MenuItem>()
+            {
+                new("1", "Liste", ts.List),
+                new("2", "Ajouter", ts.Add),
+                new("3", "Modifier", ts.Update),
+                new("4", "Supprimer", ts.Remove),
+            };
+        }
+    }
+}
--- Tacos/Program.cs
+++ Tacos/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace Tacos
@@ -8,30 +7,18 @@ namespace Tacos
     {
         static void Main(string[] args)
         {
-            var ts = new TacosService();
-            var items = new List<MenuItem>()
-            {
-                new MenuItem("1", "Liste", ts.List),
-                new MenuItem("2", "Ajouter", ts.Add),
-                new MenuItem("3", "Modifier", ts.Update),
-                new MenuItem("4", "Supprimer", ts.Remove),
-            };
-            Console.WriteLine("Que souhaitez-vous faire ? ");
-
+            var items = MenuItemFactory.GetItems();
             foreach (var menuItem in items)
             {
                 Console.WriteLine($"{menuItem.Key}/ {menuItem.Description}");
             }
 
-            string key = null;
+            string key;
             do
             {
+                Console.WriteLine("Que souhaitez-vous faire ? ");
                 key = Console.ReadLine();
-                var item = items.FirstOrDefault(x => x.Key == key);
-                if (item != null)
-                {
-                    item.Function();
-                }
+                items.FirstOrDefault(x => x.Key == key)?.Function();
             } while (key != "q");
         }
 
